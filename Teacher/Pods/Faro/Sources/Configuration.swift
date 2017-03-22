5d9c6fb018956a1e95d5251eea6fa290ccdc5646
StijnWillems <stijn.willems@icapps.com>
2017-03-22 15:22:54 +0100

    add faro

---
+++ Teacher/Pods/Faro/Sources/Configuration.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+/// Use for different configurations for the specific environment you want to use for *Bar.*
+open class Configuration {
+    open let baseURL: String
+
+    /// For now we only support JSON. Can be Changed in the future
+    open let adaptor: Adaptable
+    open var url: URL? {
+        get {
+            return URL(string: baseURL)
+        }
+    }
+
+    public init(baseURL: String, adaptor: Adaptable = JSONAdaptor()) {
+        self.baseURL = baseURL
+        self.adaptor = adaptor
+    }
+    
+}
