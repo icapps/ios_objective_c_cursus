68913f10ce34223fd24d34312b9edbeaa60cdb62
StijnWillems <stijn.willems@icapps.com>
2017-03-22 15:10:38 +0100

    adding stuff from submodule

---
+++ Objective-C/Example.swift
@@ -0,0 +1,20 @@
+//
+//  Example.swift
+//  Objective-C
+//
+//  Created by Stijn Willems on 22/03/2017.
+//  Copyright © 2017 icapps. All rights reserved.
+//
+
+import Foundation
+
+class App {
+
+	var foo: Int {
+		get {
+			return _foo
+		}
+	}
+
+	private var _foo: Int = 0
+}
