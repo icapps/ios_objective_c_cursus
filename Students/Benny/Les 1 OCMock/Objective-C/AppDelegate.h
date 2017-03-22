7fcf82824f1cbf7194356131f0f2f0313b5d3876
Ben Algoet <ben.algoet@icapps.com>
2017-03-22 15:17:51 +0100

    Copy teacher project

---
+++ Students/Benny/Les 1 OCMock/Objective-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Objective-C
+//
+//  Created by Stijn Willems on 21/03/2017.
+//  Copyright © 2017 icapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property int foo;
+
+@end
+
