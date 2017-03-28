81c064490e53a8e2baae87e05e7ec1fdb94cb28c
Ben Algoet <ben.algoet@icapps.com>
2017-03-28 11:25:58 +0200

    google drive test project

--- Students/Benny/GoogleDriveTest/GoogleDriveTest/ViewController.h
+++ Students/Benny/GoogleDriveTest/GoogleDriveTest/ViewController.h
@@ -8,8 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
+#import "GTMOAuth2ViewControllerTouch.h"
+#import "GTLRDrive.h"
+
 @interface ViewController : UIViewController
 
+@property (nonatomic, strong) GTLRDriveService *service;
+@property (nonatomic, strong) UITextView *output;
 
 @end
 
--- Students/Benny/GoogleDriveTest/GoogleDriveTest/ViewController.m
+++ Students/Benny/GoogleDriveTest/GoogleDriveTest/ViewController.m
@@ -8,22 +8,126 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
-@end
+static NSString *const kKeychainItemName = @"Drive API";
+static NSString *const kClientID = @"646986136780-ae0lrhg7jm23r8utrpv8gufoj6rnc62g.apps.googleusercontent.com";
 
 @implementation ViewController
 
+@synthesize service = _service;
+@synthesize output = _output;
+
+// When the view loads, create necessary subviews, and initialize the Drive API service.
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // Create a UITextView to display output.
+    self.output = [[UITextView alloc] initWithFrame:self.view.bounds];
+    self.output.editable = false;
+    self.output.contentInset = UIEdgeInsetsMake(20.0, 0.0, 20.0, 0.0);
+    self.output.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+    [self.view addSubview:self.output];
+    
+    // Initialize the Drive API service & load existing credentials from the keychain if available.
+    self.service = [[GTLRDriveService alloc] init];
+    self.service.authorizer =
+    [GTMOAuth2ViewControllerTouch authForGoogleFromKeychainForName:kKeychainItemName
+                                                          clientID:kClientID
+                                                      clientSecret:nil];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+// When the view appears, ensure that the Drive API service is authorized, and perform API calls.
+- (void)viewDidAppear:(BOOL)animated {
+    if (!self.service.authorizer.canAuthorize) {
+        // Not yet authorized, request authorization by pushing the login UI onto the UI stack.
+        [self presentViewController:[self createAuthController] animated:YES completion:nil];
+        
+    } else {
+        [self listFiles];
+    }
 }
 
+// List up to 10 files in Drive
+- (void)listFiles {
+    GTLRDriveQuery_FilesList *query = [GTLRDriveQuery_FilesList query];
+    query.fields = @"nextPageToken, files(id, name)";
+    query.pageSize = 10;
+    
+    [self.service executeQuery:query
+                      delegate:self
+             didFinishSelector:@selector(displayResultWithTicket:finishedWithObject:error:)];
+}
+
+// Process the response and display output
+- (void)displayResultWithTicket:(GTLRServiceTicket *)ticket
+             finishedWithObject:(GTLRDrive_FileList *)result
+                          error:(NSError *)error {
+    if (error == nil) {
+        NSMutableString *output = [[NSMutableString alloc] init];
+        if (result.files.count > 0) {
+            [output appendString:@"Files:\n"];
+            int count = 1;
+            for (GTLRDrive_File *file in result.files) {
+                [output appendFormat:@"%@ (%@)\n", file.name, file.identifier];
+                count++;
+            }
+        } else {
+            [output appendString:@"No files found."];
+        }
+        self.output.text = output;
+    } else {
+        NSMutableString *message = [[NSMutableString alloc] init];
+        [message appendFormat:@"Error getting presentation data: %@\n", error.localizedDescription];
+        [self showAlert:@"Error" message:message];
+    }
+}
+
+// Creates the auth controller for authorizing access to Drive API.
+- (GTMOAuth2ViewControllerTouch *)createAuthController {
+    GTMOAuth2ViewControllerTouch *authController;
+    // If modifying these scopes, delete your previously saved credentials by
+    // resetting the iOS simulator or uninstall the app.
+    NSArray *scopes = [NSArray arrayWithObjects:kGTLRAuthScopeDriveReadonly, nil];
+    authController = [[GTMOAuth2ViewControllerTouch alloc]
+                      initWithScope:[scopes componentsJoinedByString:@" "]
+                      clientID:kClientID
+                      clientSecret:nil
+                      keychainItemName:kKeychainItemName
+                      delegate:self
+                      finishedSelector:@selector(viewController:finishedWithAuth:error:)];
+    return authController;
+}
+
+// Handle completion of the authorization process, and update the Drive API
+// with the new credentials.
+- (void)viewController:(GTMOAuth2ViewControllerTouch *)viewController
+      finishedWithAuth:(GTMOAuth2Authentication *)authResult
+                 error:(NSError *)error {
+    if (error != nil) {
+        [self showAlert:@"Authentication Error" message:error.localizedDescription];
+        self.service.authorizer = nil;
+    }
+    else {
+        self.service.authorizer = authResult;
+        [self dismissViewControllerAnimated:YES completion:nil];
+    }
+}
+
+// Helper for showing an alert
+- (void)showAlert:(NSString *)title message:(NSString *)message {
+    UIAlertController *alert =
+    [UIAlertController alertControllerWithTitle:title
+                                        message:message
+                                 preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *ok =
+    [UIAlertAction actionWithTitle:@"OK"
+                             style:UIAlertActionStyleDefault
+                           handler:^(UIAlertAction * action)
+     {
+         [alert dismissViewControllerAnimated:YES completion:nil];
+     }];
+    [alert addAction:ok];
+    [self presentViewController:alert animated:YES completion:nil];
+    
+}
 
 @end
