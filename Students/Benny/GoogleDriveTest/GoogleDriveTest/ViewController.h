77657c1f904ee6a13dfef95979ef6cdd571f9b8f
Ben Algoet <ben.algoet@icapps.com>
2017-03-28 10:06:16 +0200

    add google drive test project

---
+++ Students/Benny/GoogleDriveTest/GoogleDriveTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoogleDriveTest
+//
+//  Created by Ben Algoet on 28/03/2017.
+//  Copyright © 2017 Ben Algoet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Students/Benny/GoogleDriveTest/GoogleDriveTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GoogleDriveTest
+//
+//  Created by Ben Algoet on 28/03/2017.
+//  Copyright © 2017 Ben Algoet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
