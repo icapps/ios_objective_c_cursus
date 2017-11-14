1da1a2e2c1cea5b62964ccc55a467a82044bb7f4
StijnWillems <stijn.willems@icapps.com>
2017-11-14 13:52:34 +0100

    post unit test

--- Teacher/ObjcTextInput/ObjcTextInput/Modules/Posts/New Group/Post.swift
+++ Teacher/ObjcTextInput/ObjcTextInput/Modules/Posts/New Group/Post.swift
@@ -2,8 +2,22 @@ import Faro
 
 class Post: NSObject, Codable {
     @objc let id: Int
+    @objc let message: String?
+    @objc let title: String
+    @objc let userId: Int
 
-    init(id: Int) {
+    init(id: Int, message: String?, title: String, userId: Int) {
         self.id = id
+        self.message = message
+        self.title = title
+        self.userId = userId
     }
+
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case message = "body"
+        case userId
+    }
+
 }
--- Teacher/ObjcTextInput/ObjcTextInput/Modules/Posts/New Group/PostTests.swift
+++ Teacher/ObjcTextInput/ObjcTextInput/Modules/Posts/New Group/PostTests.swift
@@ -19,32 +19,31 @@ class PostTests: XCTestCase {
     override func setUp() {
         super.setUp()
         jsonData = """
-        [
-            {
-            "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-            "id": 1,
-            "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-            "userId": 1
-            },
-            {
-            "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
-            "id": 2,
-            "title": "qui est esse",
-            "userId": 1
-            },
-            {
-            "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
-            "id": 3,
-            "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-            "userId": 1
-            },
-            {
-            "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit",
-            "id": 4,
-            "title": "eum et est occaecati",
-            "userId": 1
-            }
-        ]
+                [
+                  {
+                    "body": "b 1",
+                    "id": 1,
+                    "title": "",
+                    "userId": 1
+                  },
+                  {
+                    "body": "b 2",
+                    "id": 2,
+                    "title": "qui est esse",
+                    "userId": 1
+                  },
+                  {
+                    "id": 3,
+                    "title": "title",
+                    "userId": 1
+                  },
+                  {
+                    "body": "b 3",
+                    "id": 4,
+                    "title": "title ",
+                    "userId": 1
+                  }
+                ]
         """.data(using: .utf8)!
 
         let config = BackendConfiguration(baseURL:"http://www.google.com")
@@ -73,7 +72,10 @@ class PostTests: XCTestCase {
 
             let posts = try? done()
 
-            XCTAssertEqual(posts?.count, 40)
+            XCTAssertEqual(posts?.count, 4)
+            XCTAssertEqual(posts!.map {$0.id}, [1,2,3,4])
+            XCTAssertEqual(posts!.map {$0.message ?? "nothing"}, ["b 1","b 2", "nothing", "b 3"])
+
 
             expectation.fulfill()
         }
