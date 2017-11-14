523bbb1c8663c847177e0980cd0f7b4aefdfaf15
Ronald Hollander <ronaldhollander@Ronalds-MacBook-Pro.local>
2017-11-14 09:31:23 +0100

    Added to correct folder

---
+++ Students/Ronald/ObjcTextInput/ObjcTextInput/TIILabelCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  LabelCollectionViewCell.m
+//  ObjcTextInput
+//
+//  Created by Naomi De Leeuw on 10/11/2017.
+//  Copyright © 2017 iCapps. All rights reserved.
+//
+
+#import "TIILabelCollectionViewCell.h"
+
+@implementation TIILabelCollectionViewCell
+
+@end
