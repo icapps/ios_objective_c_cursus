10d55a3e1ee82d8e8ff8dd65a30afeb99954e5c7
StijnWillems <stijn.willems@icapps.com>
2017-11-16 11:04:50 +0100

    add runtime fun example

---
+++ Teacher/RuntimeFun/RuntimeFun/RuntimeFun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RuntimeFun
+//
+//  Created by Stijn Willems on 16/11/2017.
+//  Copyright © 2017 iCapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Teacher/RuntimeFun/RuntimeFun/RuntimeFun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RuntimeFun
+//
+//  Created by Stijn Willems on 16/11/2017.
+//  Copyright © 2017 iCapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
